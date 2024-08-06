953a98a1a58bab90ba7cade1c869137e98e158d5
Yury Kuzma <vterkin1388@gmail.com>
2024-08-06 14:35:06 +0300

    change User

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Objects;
+
 public class User {
     private String name;
     private String surname;
@@ -26,5 +28,18 @@ public class User {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(surname, user.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
