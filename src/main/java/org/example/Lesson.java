17e2a060ec2d754f8093c490d507cda6876ee896
Yury Kuzma <vterkin1388@gmail.com>
2024-08-08 11:16:41 +0300

    change Lesson

--- src/main/java/org/example/Lesson.java
+++ src/main/java/org/example/Lesson.java
@@ -5,4 +5,12 @@ import java.time.LocalDateTime;
 public class Lesson {
    private LocalDateTime dateTime;
    private String topic;
+
+   public Lesson(LocalDateTime dateTime, String topic) {
+      this.dateTime = dateTime;
+      this.topic = topic;
+   }
+
+   public Lesson() {
+   }
 }
