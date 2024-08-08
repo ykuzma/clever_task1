3a7f249743aa46cf3a87856119b05af6eebfcae5
Yury Kuzma <vterkin1388@gmail.com>
2024-08-08 11:17:53 +0300

    change Lesson

--- src/main/java/org/example/Lesson.java
+++ src/main/java/org/example/Lesson.java
@@ -13,4 +13,20 @@ public class Lesson {
 
    public Lesson() {
    }
+
+   public LocalDateTime getDateTime() {
+      return dateTime;
+   }
+
+   public void setDateTime(LocalDateTime dateTime) {
+      this.dateTime = dateTime;
+   }
+
+   public String getTopic() {
+      return topic;
+   }
+
+   public void setTopic(String topic) {
+      this.topic = topic;
+   }
 }
