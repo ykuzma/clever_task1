526aeb753113788da243d7d777b894a8a214ce13
Yury Kuzma <vterkin1388@gmail.com>
2024-08-08 11:14:51 +0300

    change Lecturer

--- src/main/java/org/example/Lecturer.java
+++ src/main/java/org/example/Lecturer.java
@@ -6,5 +6,8 @@ public class Lecturer {
     private String name;
     private List<List<StudyGroup>> listGroup;
 
-
+    public Lecturer(String name, List<List<StudyGroup>> listGroup) {
+        this.name = name;
+        this.listGroup = listGroup;
+    }
 }
